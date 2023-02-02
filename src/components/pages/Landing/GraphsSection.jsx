c7c0b6a95b60e8123d4db8cc9a408875f012944a
Brian Nguyen <brian.edison.nguyen@gmail.com>
2023-02-01 17:07:12 -0800

    feat: import graphs and display

--- src/components/pages/Landing/GraphsSection.jsx
+++ src/components/pages/Landing/GraphsSection.jsx
@@ -1,10 +1,24 @@
 import React from 'react';
+import GrantRatesByOfficeImg from '../../../styles/Images/bar-graph-no-text.png';
+import GrantRatesByNationalityImg from '../../../styles/Images/pie-chart-no-text.png';
+import GrantRatesOverTimeImg from '../../../styles/Images/line-graph-no-text.png';
 
 function GraphsSection() {
   return (
-    <div className="graphs-section">
-      <h1>Graphs Section</h1>
-    </div>
+    <section className="graphs-section">
+      <img
+        src={GrantRatesByOfficeImg}
+        alt="Bar chart of grant rates by office"
+      />
+      <img
+        src={GrantRatesByNationalityImg}
+        alt="Pie chart of grant rates by office"
+      />
+      <img
+        src={GrantRatesOverTimeImg}
+        alt="Line chart of grant rates over time"
+      />
+    </section>
   );
 }
 
