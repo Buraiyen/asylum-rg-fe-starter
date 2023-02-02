23f5758ea86185646831ea5fb9cdbbc71e1e3d78
Brian Nguyen <brian.edison.nguyen@gmail.com>
2023-02-02 11:10:59 -0800

    init LandingPage.test.js

---
+++ src/__tests__/LandingPage.test.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+
+import RenderLandingPage from '../components/pages/Landing/RenderLandingPage';
+
+describe('<Button /> test suite', () => {
+  test('Sanity check', () => {
+    expect(true).toBe(true);
+  });
+});
