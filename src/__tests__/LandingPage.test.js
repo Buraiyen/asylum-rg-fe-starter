5764bdbc79a0e33f7e6df4674b26985bf9439539
Brian Nguyen <brian.edison.nguyen@gmail.com>
2023-02-02 11:17:41 -0800

    test: initial button retrieval

--- src/__tests__/LandingPage.test.js
+++ src/__tests__/LandingPage.test.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import { render } from '@testing-library/react';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 import RenderLandingPage from '../components/pages/Landing/RenderLandingPage';
 
 describe('<Button /> test suite', () => {
-  test('Sanity check', () => {
-    expect(true).toBe(true);
+  test('"Data" button navigates to /graphs when clicked', () => {
+    const { getByText } = render(
+      <Router>
+        <RenderLandingPage />
+      </Router>
+    );
+    const button = getByText(/view the data/i);
+    expect(button.textContent).toBe('View the Data');
   });
 });
