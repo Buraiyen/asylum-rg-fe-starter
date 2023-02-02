e7041a9e249f2fc7cac5253ce468adb1dccdbaaf
Brian Nguyen <brian.edison.nguyen@gmail.com>
2023-02-02 10:44:54 -0800

    feat: Insights title and styles

---
+++ src/components/pages/Landing/Insights.jsx
@@ -0,0 +1,11 @@
+import './Insights.css';
+
+function Insights() {
+  return (
+    <div className="bottom-section">
+      <h2 className="title">Systemic Disparity Insights</h2>
+    </div>
+  );
+}
+
+export default Insights;
