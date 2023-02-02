f17661260c7a4be4980d3e9c87a6e54bbe574229
Brian Nguyen <brian.edison.nguyen@gmail.com>
2023-02-01 17:03:37 -0800

    init GraphsSection.jsx

---
+++ src/components/pages/Landing/GraphsSection.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function GraphsSection() {
+  return (
+    <div className="graphs-section">
+      <h1>Graphs Section</h1>
+    </div>
+  );
+}
+
+export default GraphsSection;
