cb9b936d93837e53d77fee732e0c5ba063db648f
Brian Nguyen <brian.edison.nguyen@gmail.com>
2023-02-02 11:49:30 -0800

    test: 'Read More' button render

--- src/__tests__/LandingPage.test.js
+++ src/__tests__/LandingPage.test.js
@@ -4,7 +4,7 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import RenderLandingPage from '../components/pages/Landing/RenderLandingPage';
 
 describe('<Button /> test suite', () => {
-  test('"Data" button navigates to /graphs when clicked', () => {
+  test('"Data" button renders and navigates to /graphs when clicked', () => {
     const { getByText } = render(
       <Router>
         <RenderLandingPage />
@@ -14,5 +14,17 @@ describe('<Button /> test suite', () => {
     expect(button.textContent).toBe('View the Data');
     fireEvent.click(button);
     expect(window.location.pathname).toEqual('/graphs');
+  });
+
+  test('"Read More" button renders', () => {
+    // Note: It's not possible to perform a button click or navigate to an
+    // external website because Jest runs on a simulated browser
+    const { getByText } = render(
+      <Router>
+        <RenderLandingPage />
+      </Router>
+    );
+    const button = getByText(/read more/i);
+    expect(button.textContent).toBe('Read More');
   });
 });
