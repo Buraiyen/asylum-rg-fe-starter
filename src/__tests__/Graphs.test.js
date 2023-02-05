7933d8f828a38587f45a8b11f46ef5e25d76b191
Brian Nguyen <brian.edison.nguyen@gmail.com>
2023-02-04 20:07:20 -0800

    test: TimeSeriesSingleOffice invalid data
    I included beforeEach and afterEach functions to prevent large walls of
    console errors from producing.
    
    Another test involves passing invalid data into the
    TimeSeriesSingleOffice component

--- src/__tests__/Graphs.test.js
+++ src/__tests__/Graphs.test.js
@@ -7,6 +7,17 @@ import TimeSeriesAll from '../components/pages/DataVisualizations/Graphs/TimeSer
 import TimeSeriesSingleOffice from '../components/pages/DataVisualizations/Graphs/TimeSeriesSingleOffice';
 
 describe('TimeSeries test suite', () => {
+  beforeEach(() => {
+    jest.spyOn(console, 'error');
+    // @ts-ignore jest.spyOn adds this functionallity
+    console.error.mockImplementation(() => null);
+  });
+
+  afterEach(() => {
+    // @ts-ignore jest.spyOn adds this functionallity
+    console.error.mockRestore();
+  });
+
   test('<TimeSeriesAll /> renders correctly', () => {
     const store = configureStore({ reducer: reducer });
     const { getByText } = render(
@@ -35,5 +46,16 @@ describe('TimeSeries test suite', () => {
     expect(title.textContent).toEqual(
       'Showing: Time series data for all USCIS Asylum Offices - (New York, NY)'
     );
+  });
+
+  test('<TimeSeriesSingleOffice /> fails to render when passing invalid data', () => {
+    const store = configureStore({ reducer: reducer });
+    expect(() => {
+      render(
+        <Provider store={store}>
+          <TimeSeriesSingleOffice office={null} />
+        </Provider>
+      );
+    }).toThrow("Cannot read property 'timeSeriesData' of undefined");
   });
 });
